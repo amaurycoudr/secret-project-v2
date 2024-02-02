2d94b817aba4a5fc2dd87eb086eef0f42ff11fde
amaurycoudr <amaury.coudray@gmail.com>
2024-02-02 17:23:51 +0100

    feat: V1 films of the week

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,232 @@
+import classNames from "classnames";
+import {
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+  type FC,
+  type ReactNode,
+} from "react";
+
+interface ElementBorder {
+  yStart: number;
+  yEnd: number;
+}
+interface CarouselProps<T> {
+  content: T[];
+  className: string;
+  classNameWrapperContent?: string;
+  ContentElement: FC<T & { selected: boolean }>;
+  CarouselElement: FC<T>;
+}
+
+const INITIAL_INDEX = -1;
+const Carousel = <T extends object>({
+  CarouselElement,
+  ContentElement,
+  content,
+  classNameWrapperContent,
+  className,
+}: CarouselProps<T>) => {
+  const [currentIndex, setCurrentIndex] = useState(INITIAL_INDEX);
+  const positionsRef = useRef<Record<number, ElementBorder>>({});
+
+  const updatePositionInfo = useCallback(
+    (index: number, { yEnd, yStart }: ElementBorder) => {
+      const newYEnd = yEnd + window.scrollY;
+      const newYStart = yStart + window.scrollY;
+      positionsRef.current[index] = { yEnd: newYEnd, yStart: newYStart };
+    },
+    []
+  );
+
+  const scrollToElement = useCallback((index: number) => {
+    if (positionsRef.current[index] === undefined) return;
+    const { yStart, yEnd } = positionsRef.current[index];
+    const top = Math.floor(yStart - (innerHeight - (yEnd - yStart)) / 2);
+    window.scrollTo({ top, behavior: "smooth" });
+  }, []);
+
+  const handleScroll = useCallback(() => {
+    const screenCenter = window.scrollY + window.innerHeight / 2;
+
+    const indexPositions = Object.entries(positionsRef.current);
+    if (indexPositions.length === 0) return;
+
+    const centerIndex = indexPositions.find(
+      ([, { yEnd, yStart }]) => yStart < screenCenter && screenCenter < yEnd
+    )?.[0];
+    const isBeforeFirst = indexPositions[0][1].yStart > screenCenter;
+    const isAfterLast =
+      indexPositions[indexPositions.length - 1][1].yEnd < screenCenter;
+
+    if (centerIndex === undefined && isBeforeFirst) {
+      setCurrentIndex(-1);
+    } else if (centerIndex === undefined && isAfterLast) {
+      setCurrentIndex(indexPositions.length);
+    } else if (centerIndex !== undefined) {
+      setCurrentIndex(parseInt(centerIndex[0]));
+    } else if (centerIndex === undefined && currentIndex === INITIAL_INDEX) {
+      const nearestIndex = parseInt(
+        indexPositions.sort(
+          ([, { yStart: yStart1 }], [, { yStart: yStart2 }]) =>
+            Math.min(yStart2 - screenCenter, 0) <
+            Math.min(yStart1 - screenCenter, 0)
+              ? -1
+              : 1
+        )[0][0]
+      );
+
+      setCurrentIndex(nearestIndex);
+    }
+  }, [currentIndex]);
+
+  const handleKeyDown = useCallback(
+    (ev: KeyboardEvent) => {
+      if (
+        (ev.key === " " || ev.key === "ArrowDown") &&
+        currentIndex < content.length - 1
+      ) {
+        scrollToElement(Math.min(currentIndex + 1, content.length));
+        ev.preventDefault();
+      }
+      if (ev.key === "ArrowUp" && currentIndex !== INITIAL_INDEX) {
+        scrollToElement(Math.max(currentIndex - 1, 0));
+        ev.preventDefault();
+      }
+    },
+    [currentIndex]
+  );
+
+  useEffect(() => {
+    handleScroll();
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleScroll]);
+  return (
+    <div className={classNames("flex flex-col", className)}>
+      {content.map(({ ...props }, index) => (
+        <WrapperContentElement
+          key={index}
+          index={index}
+          className={classNameWrapperContent}
+          updatePositionInfo={updatePositionInfo}
+        >
+          <ContentElement {...props} selected={index === currentIndex} />
+        </WrapperContentElement>
+      ))}
+
+      <div
+        className={classNames(
+          "fixed right-16 top-1/2 text-4xl transition-all duration-200",
+          "md:h-[250px] md:w-[187px] md:-translate-y-[125px]",
+          "lg:h-[300px] lg:w-[225px] lg:-translate-y-[150px]",
+          "xl:h-[400px] xl:w-[300px] xl:-translate-y-[200px]"
+        )}
+      >
+        {content.map((props, index) => (
+          <WrapperCarouselElement
+            key={index}
+            currentIndex={currentIndex}
+            index={index}
+            isHidden={currentIndex === -1 || currentIndex === content.length}
+            onClick={() => {
+              scrollToElement(index);
+            }}
+          >
+            <CarouselElement {...props} />
+          </WrapperCarouselElement>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const WrapperContentElement = ({
+  index,
+  updatePositionInfo,
+  className,
+  children,
+}: {
+  index: number;
+  updatePositionInfo: (index: number, info: ElementBorder) => void;
+  children: ReactNode;
+  className?: string;
+}) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const localRef = ref.current;
+    if (localRef === null) return;
+    const handleSize = () => {
+      if (localRef === null) return;
+      const { y, height } = localRef.getBoundingClientRect();
+      updatePositionInfo(index, { yStart: y, yEnd: y + height });
+    };
+
+    handleSize();
+    window.addEventListener("resize", handleSize);
+    return () => {
+      window.removeEventListener("resize", handleSize);
+    };
+  }, [updatePositionInfo, index]);
+  return (
+    <div className={className} ref={ref}>
+      {children}
+    </div>
+  );
+};
+
+const WrapperCarouselElement = ({
+  onClick,
+  currentIndex,
+  index,
+  isHidden,
+  children,
+}: {
+  children: ReactNode;
+  onClick: () => void;
+  currentIndex: number;
+  index: number;
+  isHidden: boolean;
+}) => {
+  const ref = useRef<HTMLButtonElement>(null);
+  const isFarBefore = index < currentIndex - 1;
+  const isFarAfter = index > currentIndex + 1;
+  const isBefore = index === currentIndex - 1;
+  const isAfter = index === currentIndex + 1;
+  const isCurrent = index === currentIndex;
+
+  return (
+    <button
+      onClick={onClick}
+      className={classNames(
+        "absolute -z-10 flex h-full w-full object-contain transition-all duration-200",
+        {
+          "scale-75 opacity-70 cursor-pointer": !isCurrent && !isHidden,
+          "-translate-y-full translate-x-[200%] rotate-90": isFarBefore,
+          "-translate-y-full translate-x-3/4 rotate-45 hover:scale-90 hover:opacity-100":
+            isBefore && !isHidden,
+          "translate-x-3/4 translate-y-full -rotate-45 hover:scale-90 hover:opacity-100":
+            isAfter && !isHidden,
+          "translate-x-[200%] translate-y-full -rotate-90": isFarAfter,
+          "translate-x-0 translate-y-0": isCurrent,
+          "opacity-0": isHidden,
+        }
+      )}
+      onFocus={() => {
+        ref.current?.blur();
+      }}
+      ref={ref}
+      tabIndex={-1}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Carousel;
--- src/components/FilmsOfTheWeek.tsx
+++ src/components/FilmsOfTheWeek.tsx
@@ -1,140 +1,37 @@
 import classNames from "classnames";
-import React, {
-  useEffect,
-  useRef,
-  type ReactNode,
-  useCallback,
-  useState,
-} from "react";
-import FilmsCarousel from "./FilmsCarousel";
+import { type ReactNode } from "react";
+import Carousel from "./Carousel";
+import FilmPoster from "./FilmPoster";
+
+interface FilmOfTheWeekInfos {
+  title: string;
+  id: number;
+  year: number;
+  kind: string;
+  filmmaker: string[];
+  synopsis: string;
+  rating: number;
+  aNutshell: string;
+  summary: string;
+  image: string;
+}
 
 interface Props {
-  content: Array<{
-    title: string;
-    id: number;
-    year: number;
-    kind: string;
-    filmmaker: string[];
-    synopsis: string;
-    rating: number;
-    aNutshell: string;
-    summary: string;
-    image: string;
-  }>;
+  content: FilmOfTheWeekInfos[];
 }
 
-const FilmsOfTheWeek = ({ content }: Props) => {
-  const [currentMovie, setCurrentMovie] = useState(0);
-  const positionsRef = useRef<Record<number, SectionPosition>>({});
-  console.log(currentMovie);
+const FilmsOfTheWeek = ({ content }: Props) => (
+  <Carousel
+    className="gap-24"
+    classNameWrapperContent="self-center w-full max-w-3xl md:pl-8 lg:pl-0"
+    content={content}
+    ContentElement={FilmOfTheWeek}
+    CarouselElement={({ title, image }) => (
+      <FilmPoster name={title} image={image} />
+    )}
+  />
+);
 
-  const updatePositionInfo = useCallback(
-    (id: number, { yEnd, yStart }: SectionPosition) => {
-      const newYEnd = yEnd + window.scrollY;
-      const newYStart = yStart + window.scrollY;
-
-      positionsRef.current[id] = { yEnd: newYEnd, yStart: newYStart };
-    },
-    []
-  );
-
-  const scrollToMovie = (id: number) => {
-    const top = Math.floor(
-      positionsRef.current[id].yStart -
-        (innerHeight -
-          (positionsRef.current[id].yEnd - positionsRef.current[id].yStart)) /
-          2
-    );
-
-    window.scrollTo({ top, behavior: "smooth" });
-  };
-
-  const handleScroll = useCallback(() => {
-    const screenCenter = window.scrollY + window.innerHeight / 2;
-
-    const movies = Object.entries(positionsRef.current);
-    if (movies.length === 0) return;
-
-    const idsPosition = movies.find(
-      ([, { yEnd, yStart }]) => yStart < screenCenter && screenCenter < yEnd
-    );
-    const isCurrentMovieFirst =
-      parseInt(movies[0][0]) === currentMovie &&
-      movies[0][1].yStart > screenCenter;
-    const isCurrentMovieLast =
-      parseInt(movies[movies.length - 1][0]) === currentMovie &&
-      movies[movies.length - 1][1].yEnd < screenCenter;
-
-    if (idsPosition === undefined && isCurrentMovieFirst) {
-      setCurrentMovie(-1);
-      return;
-    }
-    if (idsPosition === undefined && isCurrentMovieLast) {
-      setCurrentMovie(Infinity);
-      return;
-    }
-    if (idsPosition !== undefined) {
-      setCurrentMovie(parseInt(idsPosition[0]));
-    }
-  }, [currentMovie]);
-
-  const handleScrollEnd = useCallback(() => {
-    if (positionsRef.current[currentMovie] === undefined) return;
-    const top = Math.floor(
-      positionsRef.current[currentMovie].yStart -
-        (innerHeight -
-          (positionsRef.current[currentMovie].yEnd -
-            positionsRef.current[currentMovie].yStart)) /
-          2
-    );
-    if (top === window.scrollY) return;
-
-    window.scrollTo({ top, behavior: "smooth" });
-  }, [currentMovie]);
-
-  const movies = content.map(({ title, year, image, id }) => ({
-    title,
-    year,
-    image,
-    id,
-  }));
-  const currentMovieIndex =
-    currentMovie === -1
-      ? currentMovie
-      : currentMovie === Infinity
-      ? movies.length
-      : movies.findIndex(({ id }) => id === currentMovie);
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    handleScroll();
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [handleScroll, handleScrollEnd]);
-  return (
-    <>
-      {content.map((props) => (
-        <FilmOfTheWeek
-          {...props}
-          selected={props.id === currentMovie}
-          key={props.title + props.filmmaker.join()}
-          updatePositionInfo={updatePositionInfo}
-        />
-      ))}
-      <FilmsCarousel
-        currentIndex={currentMovieIndex}
-        onClick={scrollToMovie}
-        movies={movies}
-      />
-    </>
-  );
-};
-
-interface SectionPosition {
-  yStart: number;
-  yEnd: number;
-}
 const FilmOfTheWeek = ({
   aNutshell,
   filmmaker,
@@ -142,82 +39,42 @@ const FilmOfTheWeek = ({
   summary,
   title,
   selected,
-  updatePositionInfo,
   synopsis,
   year,
   id,
-}: {
-  title: string;
-  year: number;
-  filmmaker: string[];
-  rating: number;
-  synopsis: string;
-  aNutshell: string;
-  id: number;
+}: FilmOfTheWeekInfos & {
   selected: boolean;
-  updatePositionInfo: (id: number, position: SectionPosition) => void;
-  summary: string;
-}) => {
-  const ref = useRef<HTMLDivElement>(null);
-  console.log("----- ----- ----- ----- RENDER ----- ----- ----- -----");
-
-  useEffect(() => {
-    const localRef = ref.current;
-    if (localRef === null) return;
-    const handleSize = () => {
-      if (localRef === null) return;
-
-      const { y, height } = localRef.getBoundingClientRect();
-
-      updatePositionInfo(id, { yStart: y, yEnd: y + height });
-    };
-
-    handleSize();
-    window.addEventListener("resize", handleSize);
-    return () => {
-      window.removeEventListener("resize", handleSize);
-    };
-  }, [updatePositionInfo, id]);
-
-  return (
+}) => (
+  <div
+    className={classNames(
+      "relative flex max-w-md flex-col gap-4 pl-6 font-serif transition-all duration-300 lg:max-w-lg",
+      { "scale-90 opacity-75": !selected }
+    )}
+  >
+    <FilmHeader year={year} filmmaker={filmmaker} title={title} />
+    <LabelWrapper label="le mot">
+      <Info>{aNutshell}</Info>
+    </LabelWrapper>
+    <LabelWrapper label="la note">
+      <Info>{rating}</Info>
+    </LabelWrapper>
+    <LabelWrapper label="le résumé">
+      <Paragraph>{synopsis}</Paragraph>
+    </LabelWrapper>
+    <LabelWrapper label="l'avis">
+      <Paragraph>{summary}</Paragraph>
+    </LabelWrapper>
     <div
-      ref={ref}
-      className="w-full max-w-3xl bg-red-500 self-center md:pl-8 lg:pl-0"
-    >
-      <div
-        className={classNames(
-          "relative flex max-w-md flex-col gap-4 pl-6 font-serif transition-all duration-300 lg:max-w-lg",
-          {
-            "scale-90 opacity-75": !selected,
-          }
-        )}
-      >
-        <FilmHeader year={year} filmmaker={filmmaker} title={title} />
-        <LabelWrapper label="le mot">
-          <Info>{aNutshell}</Info>
-        </LabelWrapper>
-        <LabelWrapper label="la note">
-          <Info>{rating}</Info>
-        </LabelWrapper>
-        <LabelWrapper label="le résumé">
-          <Paragraph>{synopsis}</Paragraph>
-        </LabelWrapper>
-        <LabelWrapper label="l'avis">
-          <Paragraph>{summary}</Paragraph>
-        </LabelWrapper>
-        <div
-          className={classNames(
-            "absolute left-0 w-[2px] rounded bg-white transition-all ",
-            {
-              "h-full duration-500": selected,
-              "h-0 duration-0": !selected,
-            }
-          )}
-        />
-      </div>
-    </div>
-  );
-};
+      className={classNames(
+        "absolute left-0 w-[2px] rounded bg-white transition-all ",
+        {
+          "h-full duration-500": selected,
+          "h-0 duration-0": !selected,
+        }
+      )}
+    />
+  </div>
+);
 
 const LabelWrapper = ({
   label,
